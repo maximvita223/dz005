1c8c0d87144f8437158d0751b0561f4507a883fe
Maxim <maks.bidenko.96@mail.ru>
2022-10-12 20:20:22 +0300

    fixed

--- ex003.py
+++ ex003.py
@@ -5,9 +5,13 @@ board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
 def draw_board(board):
-    print(f'_{board[0]}_||_{board[1]}_||_{board[2]}_')
-    print(f'_{board[3]}_||_{board[4]}_||_{board[5]}_')
-    print(f' {board[6]} || {board[7]} || {board[8]} ')
+    print(f' _____________')
+    print(f'| {board[0]} || {board[1]} || {board[2]} |') 
+    print(f'|___||___||___|') 
+    print(f'| {board[3]} || {board[4]} || {board[5]} |')
+    print(f'|___||___||___|') 
+    print(f'| {board[6]} || {board[7]} || {board[8]} |')
+    print(f'|___||___||___|')
 
 
 def check_win(board):
